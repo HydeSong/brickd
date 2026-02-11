60b3629a8c01407c67f19eb54a4bc2b11edc1fe3
haifeng.song <haifeng.song@gientech.com>
2026-02-11 16:07:49 +0800

    ok

--- src/Segmented/index.tsx
+++ src/Segmented/index.tsx
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './Segmented.module.css';
 
-const Segmented = ({
-  options,
-  value: controlledValue,
-  defaultValue,
-  onChange,
-  disabled = false,
-  className,
-  style,
-}) => {
-  const [internalValue, setInternalValue] = useState(defaultValue || (options[0]?.value || ''));
+function Segmented(props) {
+  const {
+    options = [],
+    value: controlledValue,
+    defaultValue,
+    onChange,
+    disabled = false,
+    className = '',
+    style = {},
+  } = props;
+
+  const [internalValue, setInternalValue] = React.useState(defaultValue || (options[0] ? options[0].value : ''));
 
   const currentValue = controlledValue !== undefined ? controlledValue : internalValue;
 
-  const handleClick = (option) => {
+  function handleClick(option) {
     if (disabled || option.disabled) return;
     
     const newValue = option.value;
     if (controlledValue === undefined) {
       setInternalValue(newValue);
     }
-    onChange?.(newValue);
-  };
+    if (onChange) {
+      onChange(newValue);
+    }
+  }
 
-  return (
-    <div
-      className={`${styles.segmented} ${className || ''} ${disabled ? styles.disabled : ''}`}
-      style={style}
-    >
-      {options.map((option, index) => {
-        const isActive = currentValue === option.value;
-        const isDisabled = disabled || option.disabled;
-        
-        return (
-          <div
-            key={option.value}
-            className={`${styles.segmentedItem} ${isActive ? styles.active : ''} ${isDisabled ? styles.itemDisabled : ''}`}
-            style={{
-              borderLeft: index === 0 ? '1px solid #d9d9d9' : 'none',
-            }}
-            onClick={() => handleClick(option)}
-          >
-            <div className={styles.segmentedItemLabel}>{option.label}</div>
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+  return React.createElement('div', {
+    className: `${styles.segmented} ${className} ${disabled ? styles.disabled : ''}`,
+    style: style,
+  }, options.map((option, index) => {
+    const isActive = currentValue === option.value;
+    const isDisabled = disabled || option.disabled;
+    
+    return React.createElement('div', {
+      key: option.value,
+      className: `${styles.segmentedItem} ${isActive ? styles.active : ''} ${isDisabled ? styles.itemDisabled : ''}`,
+      style: {
+        borderLeft: index === 0 ? '1px solid #d9d9d9' : 'none',
+      },
+      onClick: function() {
+        handleClick(option);
+      },
+    }, React.createElement('div', {
+      className: styles.segmentedItemLabel,
+    }, option.label));
+  }));
+}
 
 export default Segmented;
